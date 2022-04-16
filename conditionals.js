7c68aefda1386c87693ff754594694352fe101f2
mdzwink <mdzwink@gmail.com>
2022-04-16 17:21:23 +0000

    Use a while loop to repeat a chorus lineD

--- conditionals.js
+++ conditionals.js
@@ -53,3 +53,141 @@ if (!raining) {
 }
 */
 
+/*let choice = 'You should go to '
+const whichSchool  = function (age) {
+  
+  //if the age is below 13 then 'Elementary school
+  //if the age is between 13 and 18 (both inclusive) then Secondary school
+  //otherwise Lighthouse labs ;P
+
+  if (age < 13)  {
+    choice += 'Elementary School!';//This code adds 'Elementary School' to the global 'choice' variable if the 'age' argument is less than 13.
+  }else if (age >=13 && age <=18) {
+    choice += 'Secondary School!';//This code adds 'Secondary School' to the global 'choice' variable if the 'age' argument is more than or equal to 13 and less than or equal to 18;
+  }else {choice += 'Lighthouse Labs!';}//This code adds 'Lighthouse Labs' to the global 'choice' variable if the 'age' argument is over 18.
+  return choice;
+}*/
+
+/*const whichSchool  = function (age) {
+  if (age < 13)  {
+    return 'Elementary School';//This code adds 'Elementary School' to the global 'choice' variable if the 'age' argument is less than 13.
+  }else if (age >=13 && age <=18) {
+    return 'Secondary School';//This code adds 'Secondary School' to the global 'choice' variable if the 'age' argument is more than or equal to 13 and less than or equal to 18;
+  }else {
+    return 'Lighthouse Labs';//This code adds 'Lighthouse Labs' to the global 'choice' variable if the 'age' argument is over 18.
+  }
+}
+
+console.log("I am 35. Which school should I go to?");
+console.log(whichSchool(35));
+console.log("I am 8. Which school should I go to?");
+console.log(whichSchool(8));
+console.log("I am 14. Which school should I go to?");
+console.log(whichSchool(14));
+*/
+
+/*___________/\/\/\*/
+
+
+
+/*var x = 1;
+
+while (x < 21) { 
+    // check divisibility
+    
+    if (x % 3 === 0 && x % 5 === 0){
+      console.log('JuliaJames');
+  }else if (x % 5 === 0) {
+      console.log('James');
+  }else if (x % 3 === 0) {
+      console.log('Julia');
+  }
+  // print Julia, James, or JuliaJames
+  // increment x
+  x = x + 1;
+} */
+
+/*................,,,,,,,................*/
+
+/*
+ * Programming Quiz: 99 Bottles of Juice (4-2)
+ *
+ * Use the following `while` loop to write out the song "99 bottles of juice".
+ * Log the your lyrics to the console.
+ *
+ * Note
+ *   - Each line of the lyrics needs to be logged to the same line.
+ *   - The pluralization of the word "bottle" changes from "2 bottles" to "1 bottle" to "0 bottles".
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a variable `num` with a starting value of `99`
+ * - Your code should include a `while` loop
+ * - Your `while` loop should have a stop condition
+ * - Your code should produce the expected output, as explained above
+ * - Your code should not be empty
+ * - BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED.
+ */
+
+
+// check value of num
+    // print lyrics using num
+    // don't forget to check pluralization on the last line!
+    // decrement num
+    
+/*
+var num = 99;
+
+while (num >= 1) {
+    if (num === 2) {
+      console.log(num + ' bottles of juice on the wall! '
+                  + num + ' bottles of juice! Take one down, pass it around... '
+                  +(num-1)+' bottle of juice on the wall!');      
+    }else if (num === 1) {
+      console.log(num + ' bottle of juice on the wall! '
+                  + num + ' bottle of juice! Take one down, pass it around... '
+                  +(num-1)+' bottles of juice on the wall!');
+    }else {
+      console.log(num + ' bottles of juice on the wall! '
+                  + num + ' bottles of juice! Take one down, pass it around... '
+                  +(num-1)+' bottles of juice on the wall!');
+    }
+    num--
+}
+*/
+
+/*__________________/\___________________*/
+
+/*
+    -Orbiter transfers from ground to internal power (T-50 seconds)
+    -Ground launch sequencer is go for auto sequence start (T-31 seconds)
+    -Activate launch pad sound suppression system (T-16 seconds)
+    -Activate main engine hydrogen burnoff system (T-10 seconds)
+    -Main engine start (T-6 seconds)
+    -Solid rocket booster ignition and liftoff! (T-0 seconds)
+ */
+var tCount = 60;
+
+while (tCount >=0) {
+  console.log('T-'+tCount + ' seconds');
+  if (tCount === 50) {
+    console.log('Orbiter transfers from ground to internal power');
+  }else if (tCount === 31) {
+    console.log('Ground launch sequencer is go for auto sequence start');
+  }else if (tCount === 16) {
+    console.log('Activate launch pad sound suppression system');
+  }else if (tCount === 10) {
+    console.log('Activate main engine hydrogen burnoff system');
+  }else if (tCount === 6) {
+    console.log('Main engine start');
+  }else if (tCount === 0) {
+    console.log('Solid rocket booster ignition and liftoff!');
+  }else {
+    console.log('T-'+tCount + ' seconds')//otherwise prints jus the countdown message
+  } 
+  tCount--
+  //countdown general
+  //countdown events messages
+  //countdown liftoff message
+}
